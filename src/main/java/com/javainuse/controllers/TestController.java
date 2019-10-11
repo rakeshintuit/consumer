4139f0099066e9f2b4f1cfa7efdfdd321e510c82
Kandpal, Rakesh <Rakesh_Kandpal@intuit.com>
2019-10-10 21:27:16 -0700

    intial commit

---
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -0,0 +1,43 @@
+package com.javainuse.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.io.IOException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import com.javainuse.model.Employee;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(value = "/getemployee", method = RequestMethod.GET)
+	public String firstPage() {
+		String baseUrl = "http://localhost:8081/employee";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response=null;
+		try{
+			response=restTemplate.exchange(baseUrl,
+					HttpMethod.GET, getHeaders(),String.class);
+		}catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
+		return response.getBody();
+	}
+
+	private static HttpEntity<?> getHeaders() throws IOException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		return new HttpEntity<>(headers);
+	}
+
+}
